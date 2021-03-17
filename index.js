d7c43f8b70a0a89f429a5f979f2c17f08eb9dd8b
Parin Jasoliya <61468785+parin23@users.noreply.github.com>
2021-03-17 15:20:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-// import { create, Client } from '@open-wa/wa-automate';
 const wa = require('@open-wa/wa-automate');
 
 wa.create().then(client => start(client));
 
 function start(client) {
-  client.onMessage(async message => {
-//    console.log(message.from);//passion id = 919328286140-1564742282@g.us
+  client.onMessage(async message => { // This function will be called when any message is detected
+    console.log(message.from);//Use this for finding the sender or reciever id
 
-    console.log(message.sender.pushname);
-   // console.log(message);
-    if (message.from == '919328286140-1564742282@g.us') {
-      await client.sendText('917984057894-1615965701@g.us', message.sender.pushname+' said:-');
-        await client.forwardMessages('917984057894-1615965701@g.us',message.id,true);
-
-    
+   console.log(message.sender.pushname); // will output sender name in console
+   console.log(message);
+    if (message.from == 'xxx82@g.us') {//enter the sender ID in this if condition for example '919328286140-1564742282@g.us'
+      await client.sendText('xx701@g.us', message.sender.pushname+' said:-');//will send name of the message sender
+        await client.forwardMessages('xx701@g.us',message.id,true);// This line will forward message , Sender -> reciever xx701@g.us is reciever
+   
     }
   });
 }
